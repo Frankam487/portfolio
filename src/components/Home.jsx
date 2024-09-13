a8ebfeb320e955f9e9f9a0fdffe65370b2e40179
frankam487 <fkamgang06@gmail.com>
2024-09-13 20:00:39 +0000

    first

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,25 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 const Home = () => {
+    const {text} = useTypewriter({
+        words: ["developper", "photographe", "designer"],
+        loop: {},
+        
+    });
     return ( 
         <div className="home">
             <div className="name-content">
                 <h4>Salut, J'suis</h4>
                 <h1>Frank Kamgang</h1>
-                <h5>Developpeur front-end</h5>
+            <h5>je sui un
+                <span >{text}</span>
+             <Cursor cursorColor="red" />
+            </h5>
             </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inciis voluptatibus iusttuyu...</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing <br /> elit. Inciis voluptatibus iusttuyu...</p>
                 <div className="btns-container">
                     <button className="tel">Telecharger CV</button>
-                    <button className="contact">Me contacter</button>
+                    <button
+                     className="contact">Me contacter</button>
                 </div>
         </div>
      );
