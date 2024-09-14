3612da9a749c0ba5751903bdb74a6001e65479bc
frankam487 <fkamgang06@gmail.com>
2024-09-14 07:43:04 +0000

    last style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,39 @@
-import { useTypewriter, Cursor } from "react-simple-typewriter";
-import Icons from "./Icons";
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import Icons from './Icons';
+
+
 const Home = () => {
-    const {text} = useTypewriter({
-        words: ["developper", "photographe", "designer"],
-        loop: {},
-        
+    const { text } = useTypewriter({
+        words: ['développeur', 'photographe', 'designer'],
+        loop: {}, 
+        typeSpeed: 100,
+        deleteSpeed: 50, 
+        delaySpeed: 1500
     });
-    return ( 
+
+    return (
         <div className="home">
-            <Icons/>
+            <Icons />
             <div className="name-content">
-                <h4>Salut, J'suis</h4>
+                <h4>Salut, je suis</h4>
                 <h1>Frank Kamgang</h1>
-            <h5>je sui un
-                <span >{text}</span>
-             <Cursor cursorColor="red" />
-            </h5>
+                <h5>
+                    Je suis un
+                    <span>
+                        {text}
+                    </span>
+                    <Cursor />
+                </h5>
             </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing <br /> elit. Inciis voluptatibus iusttuyu...</p>
-                <div className="btns-container">
-                    <button className="tel">Telecharger CV</button>
-                    <button
-                     className="contact">Me contacter</button>
-                </div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inciis voluptatibus iusttuyu...
+            </p>
+            <div className="btns-container">
+                <button className="tel">Télécharger CV</button>
+                <button className="contact">Me contacter</button>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
