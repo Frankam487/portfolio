cd994f0a707be1bdd0da9a090980f078f7b1b32b
Frankam487 <fkamgang06@gmail.com>
2024-09-28 22:51:37 -0700

    form-contact styled

--- src/components/Formm.jsx
+++ src/components/Formm.jsx
@@ -1,91 +1,96 @@
 import React from "react";
 
 function Formm() {
-
   return (
-    <div className="grid items-center justify-center bg-gray-250   grid-cols-[60%_40%] gap-4 w-full p-6 bg-white rounded shadow-md">
-      <form className="w-full  p-5 bg-white rounded shadow-md">
-        <div className="flex justify-between gap-4">
-          <div className="flex-1">
-            <input
-              type="text"
-              id="input1"
-              className="p-2 border border-gray-300 rounded w-full"
-              placeholder="Entrez la première valeur"
+    <>
+      <div className="flex flex-col items-center bg-white pt-12">
+        <h2 className="text-blue-400 font-semibold">Me Contacter</h2>
+        <h1 className="text-blue-900 font-extrabold text-3xl">Get in Touch</h1>
+      </div>
+      <div className="grid items-center justify-center bg-gray-250   grid-cols-[60%_40%] gap-4 w-full p-6 bg-white rounded shadow-md">
+        <form className="w-full  p-5 bg-white rounded shadow-md">
+          <div className="flex justify-between gap-4">
+            <div className="flex-1">
+              <input
+                type="text"
+                id="input1"
+                className="p-2 border border-gray-300 rounded w-full"
+                placeholder="Entrez la première valeur"
+              />
+            </div>
+
+            <div className="flex-1">
+              <input
+                type="text"
+                id="input2"
+                className="p-2 border border-gray-300 rounded w-full"
+                placeholder="Entrez la seconde valeur"
+              />
+            </div>
+          </div>
+
+          <input
+            type="text"
+            id="inputMiddle"
+            className="p-2 my-2 border border-gray-300 rounded w-full"
+            placeholder="Entrez une valeur"
+          />
+
+          <textarea
+            id="textarea"
+            className="p-2 my-2 border border-gray-300 rounded w-full"
+            rows="4"
+            placeholder="Entrez une description"
+          ></textarea>
+
+          <button
+            type="submit"
+            className="mt-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-50"
+          >
+            Soumettre
+          </button>
+        </form>
+        <div className="py-10 flex flex-col">
+          <div className=" m-2 flex items-center  ml-12">
+            <img
+              src=""
+              alt=""
+              className="w-2 p-7 h-2  rounded-full bg-blue-500"
             />
+            <div className="flex flex-col ml-7 justify-start w-full">
+              <h3 className="font-bold font-arial">Call Me</h3>
+              <span className="text-start">690-461-830</span>
+              <span className="">677-331-000</span>
+            </div>
           </div>
-
-          <div className="flex-1">
-            <input
-              type="text"
-              id="input2"
-              className="p-2 border border-gray-300 rounded w-full"
-              placeholder="Entrez la seconde valeur"
+          <div className="flex items-center  ml-12 ">
+            <img
+              src="."
+              alt=""
+              className="w-2 p-7 h-2  rounded-full bg-blue-500"
             />
+            <div
+              className="flex flex-col ml-7 justify-start w-full"
+            >
+              <h3 className="font-Arial text-start font-bold text-lg">E-mail</h3>
+              <span className="text-start">fkamgang06@gmail.com</span>
+            </div>
           </div>
-        </div>
-
-        <input
-          type="text"
-          id="inputMiddle"
-          className="p-2 my-2 border border-gray-300 rounded w-full"
-          placeholder="Entrez une valeur"
-        />
-
-        <textarea
-          id="textarea"
-          className="p-2 my-2 border border-gray-300 rounded w-full"
-          rows="4"
-          placeholder="Entrez une description"
-        ></textarea>
-
-        <button
-          type="submit"
-          className="mt-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-50"
-        >
-          Soumettre
-        </button>
-      </form>
-      <div className="py-10 flex flex-col">
-        <div className=" m-2 flex items-center float-end ml-12">
-          <img
-            src=""
-            alt=""
-            className="w-2 p-7 h-2  rounded-full bg-blue-500"
-          />
-          <div className="flex items-center w-19 ml-8 flex-col ">
-            <h3 className="font-bold font-arial">Call Me</h3>
-            <span className="">201-354-4443</span>
-            <span className="">201-354-4443</span>
-          </div>
-        </div>
-        <div className="flex items-center float-end ml-12">
-          <img
-            src="."
-            alt=""
-            className="w-2 p-7 h-2  rounded-full bg-blue-500"
-          />
-          <div className="flex  items-center w-19 ml-8
-           flex-col">
-            <h3 className="font-bold font-Arial">Call Me</h3>
-            <span>201-354-4443</span>
-            <span>201-354-4443</span>
-          </div>
-        </div>
-        <div className="flex items-center float-end m-3 ml-12">
-          <img
-            src="."
-            alt=""
-            className="w-2 p-7 h-2 rounded-full bg-blue-500"
-          />
-          <div className="flex items-center flex-col ml-8 w-19 ">
-            <h3 className="font-bold">Call Me</h3>
-            <span>201-354-4443</span>
-            <span>201-354-4443</span>
+          <div className="flex items-center  m-3 ml-12 ">
+            <img
+              src="."
+              alt=""
+              className="w-2 p-7 h-2 rounded-full bg-blue-500"
+            />
+            <div className="flex flex-col ml-7 justify-start w-full">
+              <h3 className="font-bold">Call Me</h3>
+              <span className="text-start">201-354-4443</span>
+              <span>201-354-4443</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
