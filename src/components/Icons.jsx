ab6b33c5d2c8a6b5478ffcb4a56bbcab8720b00b
Frankam487 <fkamgang06@gmail.com>
2024-11-01 14:41:18 -0700

    test

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -8,6 +8,7 @@ const Icons = () => {
    }, [])
    return (
       <div className="icons">
+         
          <a href="https://www.facebook.com/Frankam.Shopping" target="_blank" className="fab fa-facebook"></a>
          <a href="https://github.com/Frankam487" target="_blank" className="fab fa-github"></a>
          <a href="https://www.linkedin.com/in/frank-kamgang/" target="_blank" className="fab fa-linkedin"></a>
